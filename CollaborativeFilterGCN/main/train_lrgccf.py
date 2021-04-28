64482a7dcaccfebca7e99c5857b5061d09c4b441
zhanchao019 <34484847+zhanchao019@users.noreply.github.com>
2021-04-29 01:11:14 +0800

    添加了根据不同数据集生成相应的权重文件的选项

--- CollaborativeFilterGCN/main/train_lrgccf.py
+++ CollaborativeFilterGCN/main/train_lrgccf.py
@@ -92,5 +92,5 @@ if __name__ == '__main__':
             print("recall@10:[{:.6f}], ndcg@10:[{:.6f}], recall@20:[{:.6f}], ndcg@20:[{:.6f}]".format(*perf_info))
             
             # save embedding
-            torch.save((user_emb, item_emb), f=args.parameters_path + '_' + str(epoch + 1) + '.pth')
+            torch.save((user_emb, item_emb), f=args.parameters_path + '_' + args.dataset_name + '_' + str(epoch + 1) + '.pth')
     f.close()
